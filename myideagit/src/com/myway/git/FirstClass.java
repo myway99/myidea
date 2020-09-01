d88738d83303c96062c59d07a0f79580fa3c2925
myway <myway99@sina.com>
2020-09-01 19:52:55 +0800

    first commit project to git

---
+++ myideagit/src/com/myway/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.myway.git;
+
+public class FirstClass {
+
+  public static void main(String[] args) {
+
+    System.out.println("hello git project.");
+    System.out.println("hello music.");
+    System.out.println("hello friend.");
+  }
+
+}
